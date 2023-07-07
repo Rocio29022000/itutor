d8cc07bb530918ed1acf1eee60e8a967ac00e8f0
Rocio Carmona <rociodelcarmen29022000@gmail.com>
2023-07-07 11:17:03 +0100

    update

--- JavaScript/billie.js
+++ JavaScript/billie.js
@@ -1,64 +1,27 @@
-"use strict"
-console.log ("Fill it in!")
+"use strict";
 
-const questionAnswer = document.getElementById("answer-area");
-const question = document.getElementById("question");
-
-//empty array to push new questions when created
-let allQuestions = [];
-let questions = ["01", "02"]
-
-//Random Number generator CHECK IF QUESTIONS OR ALL QUESTIONS FOR THE LENGTH
-function randomNum(){
-  return Math.floor(Math.random() * allQuestions.length);
+function checkTheme() {
+  const localStoreTheme = localStorage.getItem("Theme");
+  if (localStorage !== null && localStoreTheme === "dark") {
+    document.body.className = localStoreTheme;
+    themeSwitch.checked = true;
+  } else {
+    themeSwitch.checked = false;
+  }
 }
 
-
-//FOR NOW: Construct Fill in questions
-function Fill(questionName, src, alt) {
-  this.questionName = questionName;
-  this.src = src;
-  this.alt = alt;
-  console.log(this.questionName);
-  console.log(this.src);
-  allQuestions.push(this)
+checkTheme();
+function clickHandler() {
+  console.log("click");
+  if (this.checked) {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    localStorage.setItem("Theme", "dark");
+  } else {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    localStorage.setItem("Theme", "light");
+  }
 }
 
-for (let i=0; i < questions.length; i++){
-  new Fill(questions[i], `/Assets/Maths/c1/Easy/Fill/${questions[i]}.png`);
-
-}
-
-function renderQ(){
-  let q1 = randomNum();
-  console.log(q1);
-  question.src = allQuestions[q1].src;
-  question.alt = allQuestions[q1].alt;
-
-}
-renderQ();
-
-  // let questionSection1 = prompt ("")
-  //   if (questionSection1 === "2" && questionSection2 === "4" && questionSection3 = "c") 
-  //   questionAnswer = true
-   
-    // q1 = new Fill(blah,blah.blah, [correctansw1, correctansw2,])
-
-    // <p>E=M <input type='text' id='q1' maxlength='1' size='2'/> ^2 </p>
-
-
-
-// fill-in-the-blank                    // some default input if the student doesn't enter anything
-// say three options for student [] = M[]<sup>[]</sup>
-// in object, save full correct answer in array ['E','C','2'] - use stringify, and toUpperCase
-
-
-//mark = 0
-// helper function (studentAns, correctInput) {
-// for correctInput {
-//    studentAns[i] === conrrectInput[i]
-        //mark++
-//}   
-//}
-
-//mcq 
+themeSwitch.addEventListener("click", clickHandler);
--- JavaScript/rocio.js
+++ JavaScript/rocio.js
@@ -1,73 +1,371 @@
 "use strict"
-console.log("'Tis the MCQ")
- 
-let ro = "cool"
+console.log("This is a test")
 
 //Target elements
-const questionName = document.getElementById("question-area");
+const questionArea = document.getElementById("question-area");
+const questionAnswer = document.getElementById("answer-area");
 const question = document.getElementById("question");
 
+//Set number of questions for test
+let maxQuestions = 5; 
+let questionOn = 1;
+let previousQuestion = 0;
+let score = 0;
+
 //empty array to push new questions when created
-let allQuestions = [];
-let questions = ["01", "02"]
+let testQuestions = [];
+let shownQuestions = [];
+let allResults = [];
+let testN = 0;
+let possibleQuestions = ["diff-01", "diff-02", "int-01", "int-02", "surd-01"];
+let correctAnswers = [31, -2, 0.5, 4, 2, 4, "c", 2, -2, 3, 3, 2];
+let diff01a = [31];
+let diff02a = [-2, 0.5, 4];
+let int01a = [2, 4, "c"];
+let int02a = [2, -2, 3];
+let surd01a = [3, 2];
+let answers = [];
 
 //Random Number generator CHECK IF QUESTIONS OR ALL QUESTIONS FOR THE LENGTH
 function randomNum(){
-    return Math.floor(Math.random() * allQuestions.length);
+    // return Math.floor(Math.random() * testQuestions.length);
+    return Math.floor(Math.random()*possibleQuestions.length);
+
 }
 
-//Constructor function for new object (MCQ question)
-function MCQ(questionName, src, alt) {
-    this.questionName = questionName;
+  //Construct Fill in questions
+function Fill(name, src) {
+    this.name = name;
     this.src = src;
-    this.alt = alt;
-    console.log(this.questionName);
-    console.log(this.src);
-    allQuestions.push(this)
-    // this.renderQ();
+    testQuestions.push(this)
   }
 
-  
 // Create new instances of an object (create new question)
-for (let i=0; i < questions.length; i++){
-    new MCQ(questions[i], `/Assets/Maths/c1/Easy/MCQ/${questions[i]}.png`);
+for (let i=0; i < possibleQuestions.length; i++){
+    new Fill(possibleQuestions[i], `/Assets/Maths/c1/Easy/Fill/${possibleQuestions[i]}.png`);
 }
 
-function renderQ(){
+// Rendering the questions
+function renderFillQ(){
+  testN = JSON.parse(localStorage.getItem("testNumber"))
+    while(questionAnswer.firstChild){
+        questionAnswer.removeChild(questionAnswer.firstChild)
+    }
+
+    let questionH = document.createElement("h2");
+    questionH.textContent = `Question ${questionOn}:`
+    questionAnswer.appendChild(questionH)
+
     let q1 = randomNum();
-    console.log(q1);
-    question.src = allQuestions[q1].src;
-    question.alt = allQuestions[q1].alt;
+    question.src = testQuestions[q1].src;
+    question.alt = testQuestions[q1].name;
+    
+    console.log(testQuestions[q1].name)
 
+    //Avoid repetition
+    while (shownQuestions.includes(q1)){
+        q1 = randomNum();
+    }
+
+    shownQuestions.push(q1);
+    console.log(shownQuestions)
+
+    if (testQuestions[q1].name == "diff-01"){
+        answerDiff01();
+    } else if (testQuestions[q1].name == "diff-02"){
+        answerDiff02();
+    }else if (testQuestions[q1].name == "int-01"){
+        answerInt01();
+    }else if (testQuestions[q1].name == "int-02"){
+        answerInt02();
+    }else {
+        answerSurd01();
+    }
+
+    let button1 = document.createElement("BUTTON");
+    let next = document.createTextNode("Next");
+    button1.appendChild(next)
+    questionAnswer.appendChild(button1)
+    let button2 = document.createElement("BUTTON");
+    let finish = document.createTextNode("Finish");
+    button2.appendChild(finish)
+    questionAnswer.appendChild(button2)
+
+    if (questionOn == maxQuestions){
+        questionAnswer.removeChild(button1)
+    } else if (questionOn < maxQuestions){
+        questionAnswer.removeChild(button2)
+    }
+    else{
+        questionAnswer.removeChild(button1)
+        questionAnswer.removeChild(button2)
+    }
+    previousQuestion = questionOn -1
+    questionOn ++;
+
+    button1.onclick = function() {nextB()};
+    button2.onclick = function() {finishB()};
 }
-renderQ();
+renderFillQ();
 
+function answerDiff01(){
+    let form = document.createElement("FORM");
+    form.setAttribute("type", "text");
+    let fieldset = document.createElement("FIELDSET")
+    let legend = document.createElement("LEGEND")
+    legend.textContent = " Answer Area: "
+    let label = document.createElement("LABEL")
+    label.textContent = "Your Answer: "
+    let input = document.createElement("INPUT")
+    input.setAttribute("type", "number")
+    fieldset.appendChild(legend)
+    fieldset.appendChild(label)
+    fieldset.appendChild(input)
+    form.appendChild(fieldset)
+    questionAnswer.appendChild(form)
+}
+function answerDiff02(){
+    let form = document.createElement("FORM");
+    form.setAttribute("type", "text");
+    let fieldset = document.createElement("FIELDSET")
+    let legend = document.createElement("LEGEND")
+    legend.textContent = " Answer Area: "
+    let label1 = document.createElement("LABEL")
+    label1.textContent = "Gradient: "
+    let input1 = document.createElement("INPUT")
+    let label2 = document.createElement("LABEL")
+    label2.textContent = "m: "
+    let input2 = document.createElement("INPUT")
+    let label3 = document.createElement("LABEL")
+    label3.textContent = "c: "
+    let input3 = document.createElement("INPUT")
+    input1.setAttribute("type", "number")
+    input2.setAttribute("type", "number")
+    input3.setAttribute("type", "number")
+    fieldset.appendChild(legend)
+    fieldset.appendChild(label1)
+    fieldset.appendChild(input1)
+    fieldset.appendChild(label2)
+    fieldset.appendChild(input2)
+    fieldset.appendChild(label3)
+    fieldset.appendChild(input3)
+    form.appendChild(fieldset)
+    questionAnswer.appendChild(form)
+}
+function answerInt01(){
+    let form = document.createElement("FORM");
+    form.setAttribute("type", "text");
+    let fieldset = document.createElement("FIELDSET")
+    let legend = document.createElement("LEGEND")
+    legend.textContent = " Answer Area: "
+    let input1 = document.createElement("INPUT")
+    let label1 = document.createElement("LABEL")
+    label1.innerHTML = "x<sup>4</sup> +"
+    let input2 = document.createElement("INPUT")
+    let label2 = document.createElement("LABEL")
+    label2.textContent = "x + "
+    let input3 = document.createElement("INPUT")
+    input1.setAttribute("type", "number")
+    input2.setAttribute("type", "number")
+    input3.setAttribute("type", "text")
+    fieldset.appendChild(legend)
+    fieldset.appendChild(input1)
+    fieldset.appendChild(label1)
+    fieldset.appendChild(input2)
+    fieldset.appendChild(label2)
+    fieldset.appendChild(input3)
+    form.appendChild(fieldset)
+    questionAnswer.appendChild(form)
+}
+function answerInt02(){
+    let form = document.createElement("FORM");
+    form.setAttribute("type", "text");
+    let fieldset = document.createElement("FIELDSET")
+    let legend = document.createElement("LEGEND")
+    legend.textContent = " Answer Area: "
+    let input1 = document.createElement("INPUT")
+    let label1 = document.createElement("LABEL")
+    label1.innerHTML = "x<sup>6</sup> +"
+    let input2 = document.createElement("INPUT")
+    let label2 = document.createElement("LABEL")
+    label2.innerHTML = "x<sup>4</sup> +"
+    let input3 = document.createElement("INPUT")
+    let label3 = document.createElement("LABEL")
+    label3.textContent = "x + c"
+    input1.setAttribute("type", "number")
+    input2.setAttribute("type", "number")
+    input3.setAttribute("type", "text")
+    fieldset.appendChild(legend)
+    fieldset.appendChild(input1)
+    fieldset.appendChild(label1)
+    fieldset.appendChild(input2)
+    fieldset.appendChild(label2)
+    fieldset.appendChild(input3)
+    fieldset.appendChild(label3)
+    form.appendChild(fieldset)
+    questionAnswer.appendChild(form)
+}
+function answerSurd01(){
+    let form = document.createElement("FORM");
+    form.setAttribute("type", "text");
+    let fieldset = document.createElement("FIELDSET")
+    let legend = document.createElement("LEGEND")
+    legend.textContent = " Answer Area: "
+    let label1 = document.createElement("LABEL")
+    label1.textContent = "a: "
+    let input1 = document.createElement("INPUT")
+    let label2 = document.createElement("LABEL")
+    label2.textContent = "b: "
+    let input2 = document.createElement("INPUT") 
+    input1.setAttribute("type", "number")
+    input2.setAttribute("type", "number")
+    fieldset.appendChild(legend)
+    fieldset.appendChild(label1)
+    fieldset.appendChild(label2)
+    fieldset.appendChild(input1)
+    fieldset.appendChild(label2)
+    fieldset.appendChild(input2)
+    form.appendChild(fieldset)
+    questionAnswer.appendChild(form)
+}
 
+//Function for the next question
+function nextB(){
+    retrieve();
+    console.log("Next question please!")
+    renderFillQ();
+}
+//Function for results page
+function finishB(){
+  // retrieve()
+  console.log("You finished the test, YAY!")
 
+  results()
+}
 
+// Function to retrieve the user input
+function retrieve(){
+    let userAnswer = questionAnswer.getElementsByTagName("INPUT")
+    let index = shownQuestions[previousQuestion];
+    let compare = testQuestions[index].name
+    console.log(compare)
+    if (compare == "diff-01"){
+        console.log("This is the first differential Q!")
+        for (let i = 0; i < userAnswer.length; i++){
+          console.log(`The answer is ${diff01a[i]} and the user input was ${userAnswer[i].value}`)
+          if (diff01a[i] == userAnswer[i].value){
+            answers.push("correct")
+            score ++
+          } else{
+            answers.push("incorrect")
+          }
+          console.log(answers[i])
+        }
+      }
+      else if (compare == "diff-02"){
+        console.log("This is the second differential Q!")
+        for (let i = 0; i < userAnswer.length; i++){
+          if (diff02a[i] == userAnswer[i].value){
+            answers.push("correct")
+            score ++
+          } else{
+            answers.push("incorrect")
+          }
+          console.log(answers[i])
+        }
+      }
+      else if (compare == "int-01"){
+        console.log("This is the first integration Q!")
+        for (let i = 0; i < userAnswer.length; i++){
+          if (int01a[i] == userAnswer[i].value){
+            answers.push("correct")
+            score ++
+        } else{
+            answers.push("incorrect")
+        }
+        console.log(answers[i])
+        }
+      }
+      else if (compare == "int-02"){
+        console.log("This is the second integration Q!")
+        for (let i = 0; i < userAnswer.length; i++){
+          if (int02a[i] == userAnswer[i].value){
+            answers.push("correct")
+            score ++
+        } else{
+            answers.push("incorrect")
+        }
+        console.log(answers[i])
+        }
+      }
+      else if (compare == "surd-01"){
+        console.log("This is the first surds Q!")
+        for (let i = 0; i < userAnswer.length; i++){
+          if (surd01a[i] == userAnswer[i].value){
+            answers.push("correct")
+            score ++
+        } else{
+            answers.push("incorrect")
+        }
+        console.log(answers[i])
+        }
+      }
+            
+            console.log("The score is " + score)
+}
+    
+function results(){
+  //Remove content
+  questionAnswer.remove()
+  question.remove()
 
+  let message = document.createElement("h2")
+  message.textContent = "Here are your Results!"
+  questionArea.appendChild(message)
 
+  let stat = Math.round((score/correctAnswers.length)*100);
+  let result = document.createElement("p")
+  result.textContent = `You scored ${score} out of ${correctAnswers.length}, which is ${stat}%.`
+  questionArea.appendChild(result)
 
+  let chart = document.createElement("canvas");
+  chart.id = "MyChart"
+  questionArea.appendChild(chart)
 
+  let right = score;
+  let wrong = correctAnswers.length - score;
 
-//Random number to select what question is shown
-// function randomNum(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-//   }
+  const data = {
+    labels: [
+      'Correct',
+      'Incorrect',
+    ],
+    datasets: [{
+      label: 'Results',
+      data: [right, wrong],
+      backgroundColor: [
+        'green',
+        'red',
+      ],
+      hoverOffset: 4
+    }]
+  };
+  const config = {
+    type: 'doughnut',
+    data: data,
+  };
+  
+  const resultsChart = document.getElementById("MyChart");
+  let myChart = new Chart(resultsChart, config)
 
-// Constructor function to create new Question
-// with each question I want to create a question area and an answer area.
+  setLocalStorage();
+}
 
-
-
-// MCQ.prototype.renderQ = function(){
-//     this.questionName = questions[randomNum(0,1)]
-// }
-
-// function renderQ(){
-//     let question = questions[randomNum(0,1)];
-//     console.log (question)
-// }
-// renderQ();
-
-// const question1 = new MCQ(question, )
+function setLocalStorage(){
+  console.log(typeof(testN))
+  testN++
+  localStorage.setItem("testNumber", JSON.stringify(testN));
+  localStorage.setItem(`Results ${testN}`, JSON.stringify(score));
+  console.log("Score stored")
+  console.log(testN)
+}
